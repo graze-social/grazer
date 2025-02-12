1eedac381598cc04d4a51f52ed2d4439835c2681
Devin Gaffney <itsme@devingaffney.com>
2025-02-12 12:58:39 -0800

    reverse order when targeting on NSFWness

--- app/algos/operators/image_nsfw.py
+++ app/algos/operators/image_nsfw.py
@@ -5,9 +5,12 @@ class ImageNSFWParser(ImageParser):
     MODEL_NAME = "Falconsai/nsfw_image_detection"
 
     async def probability_function(self, cache_keys, images, category):
-        return await self.gpu_classifier_worker.image_nsfw_classify.remote(
+        responses = await self.gpu_classifier_worker.image_nsfw_classify.remote(
             self.MODEL_NAME, cache_keys, images
         )
+        if category == "NSFW":
+            return [list(reversed(e)) for e in responses]
+        return responses
 
     async def register_operations(self, logic_evaluator):
         await logic_evaluator.add_operation(
