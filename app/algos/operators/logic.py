b4a23eb9ecfbf2229518f3c5a441854c9d2f89fb
Devin Gaffney <itsme@devingaffney.com>
2025-03-28 13:02:18 -0700

    fix

--- app/algos/operators/logic.py
+++ app/algos/operators/logic.py
@@ -1,6 +1,6 @@
 import numpy as np
 from app.algos.base import BaseParser
-
+from app.logic_evaluator import LogicEvaluator
 
 class LogicParser(BaseParser):
     async def param_compare(self, records, input_value, operator, value):
