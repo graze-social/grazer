0ee7836df6d6bbeca8e9140dda014c9cc312eef7
Devin Gaffney <itsme@devingaffney.com>
2025-04-01 10:10:24 -0700

    fix param compare node

--- app/algos/operators/logic.py
+++ app/algos/operators/logic.py
@@ -5,7 +5,7 @@ from app.logic_evaluator import LogicEvaluator
 class LogicParser(BaseParser):
     async def param_compare(self, records, input_value, operator, value):
         return np.array(
-            LogicEvaluator.compare([input_value for e in records], operator, value),
+            LogicEvaluator.compare(np.array([input_value for e in records]), operator, value),
             dtype=bool,
         )
 
